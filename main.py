2d0ea28000a5baad0de565d6a4e3bbb48c285c5e
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-28 19:05:02 +0000

    [Readability] elem => element

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Imports 
-import boto3
-import csv
-import os
+import boto3, csv, os
 from datetime import datetime
-
+ 
 iam = boto3.client("iam")
 
 userList = []
 userAccountInfo = [['User', 'Date Created', 'Password Last Used']] 
 accessKeyInfo = [['User', 'Access Key ID', 'Status', 'Date Created', 'Last Used']]
-
 
 # Get list of users
 paginator = iam.get_paginator('list_users')
@@ -25,27 +22,22 @@ for response in paginator.paginate():
             newRow.append("Password Never Used")
         userAccountInfo.append(newRow)
 
-
-
 # Get Access Keys from list of users
 for item in userList:
     paginator = iam.get_paginator('list_access_keys')
     for response in paginator.paginate(UserName=item):
-        for elem in response['AccessKeyMetadata']:
-            lastUsedInfo = iam.get_access_key_last_used(AccessKeyId=elem['AccessKeyId'])
+        for element in response['AccessKeyMetadata']:
+            lastUsedInfo = iam.get_access_key_last_used(AccessKeyId=element['AccessKeyId'])
             newEntry = []
-            newEntry.append(elem['UserName'])
-            newEntry.append(elem['AccessKeyId'])
-            newEntry.append(elem['Status'])
-            newEntry.append(elem['CreateDate'].strftime("%d/%m/%Y"))
+            newEntry.append(element['UserName'])
+            newEntry.append(element['AccessKeyId'])
+            newEntry.append(element['Status'])
+            newEntry.append(element['CreateDate'].strftime("%d/%m/%Y"))
             if 'LastUsedDate' in lastUsedInfo['AccessKeyLastUsed']:
                 newEntry.append(lastUsedInfo['AccessKeyLastUsed']['LastUsedDate'].strftime("%d/%m/%Y"))
             else:
                 newEntry.append("Key Never Used")
             accessKeyInfo.append(newEntry)
-
-
-
 
 # Create CSV's
 # Console Access CSV
